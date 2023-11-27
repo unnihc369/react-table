d32710755fc57c6cb7548440f2989db0d3e79392
unnihc <unnihc369@gmail.com>
2023-11-27 18:11:28 +0530

    Added loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { Box, Select, MenuItem } from "@mui/material";
+import {
+  Box,
+  Select,
+  MenuItem,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
@@ -16,32 +22,48 @@ const columns = [
 ];
 
 const fetchData = async (numResults) => {
-  const response = await fetch(
-    `https://randomuser.me/api/?results=${numResults}`
-  );
-  const data = await response.json();
+  try {
+    const response = await fetch(
+      `https://randomuser.me/api/?results=${numResults}`
+    );
+    const data = await response.json();
 
-  return data.results.map((result, index) => ({
-    id: index + 1,
-    city: result.location.city,
-    state: result.location.state,
-    country: result.location.country,
-    postcode: result.location.postcode,
-    streetNumber: result.location.street.number,
-    name: `${result.name.title} ${result.name.first} ${result.name.last}`,
-    latitude: result.location.coordinates.latitude,
-    longitude: result.location.coordinates.longitude,
-  }));
+    if (!response.ok) {
+      throw new Error(data.error || "Failed to fetch data");
+    }
+
+    return data.results.map((result, index) => ({
+      id: index + 1,
+      city: result.location.city,
+      state: result.location.state,
+      country: result.location.country,
+      postcode: result.location.postcode,
+      streetNumber: result.location.street.number,
+      name: `${result.name.title} ${result.name.first} ${result.name.last}`,
+      latitude: result.location.coordinates.latitude,
+      longitude: result.location.coordinates.longitude,
+    }));
+  } catch (error) {
+    throw new Error(`Error fetching data: ${error.message}`);
+  }
 };
 
 const App = () => {
   const [rows, setRows] = useState([]);
   const [numResults, setNumResults] = useState(20);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getData = async () => {
-      const data = await fetchData(numResults);
-      setRows(data);
+      try {
+        const data = await fetchData(numResults);
+        setRows(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     };
 
     getData();
@@ -62,26 +84,35 @@ const App = () => {
     <div className="container">
       <Box sx={{ marginBottom: 2 }}></Box>
       <div className="flex">
-        <button className="button" onClick={downloadAsPDF}>
+        <button className="button" onClick={downloadAsPDF} disabled={loading}>
           Download as PDF
         </button>
         <Select
           value={numResults}
           onChange={(e) => setNumResults(Number(e.target.value))}
+          disabled={loading}
         >
           <MenuItem value={20}>20 Users</MenuItem>
           <MenuItem value={50}>50 Users</MenuItem>
         </Select>
       </div>
-      <Box id="data-grid">
-        <DataGrid
-          rows={rows}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5, 10, 20]}
-          className="data-rows"
-        />
-      </Box>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Typography variant="body1" color="error">
+          {error}
+        </Typography>
+      ) : (
+        <Box id="data-grid">
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5, 10, 20]}
+            className="data-rows"
+          />
+        </Box>
+      )}
     </div>
   );
 };
